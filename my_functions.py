0692458051e88f3b609913265558ee149a0779bc
DevSharpknife <davidjamesmurphy@gmail.com>
2020-09-08 21:23:46 +0100

    Modified function to use variables as arguments and added another example

--- my_functions.py
+++ my_functions.py
@@ -1,7 +1,14 @@
 def greet(name, time_of_day):
     return "Good " + time_of_day + ", " + name
 
-greeting = greet("Rowan", "morning")
+name_1 = "Rowan"
+time_of_day_1 = "morning"
+greeting = greet(name_1, time_of_day_1)
 print(greeting)
-
 # => "Good morning, Rowan"
+
+name_2 = "Frank"
+time_of_day_2 = "night"
+greeting = greet(name_2, time_of_day_2)
+print(greeting)
+# => "Good night, Frank"
