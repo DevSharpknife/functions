7fd7dc1e8d4267023f63cb071e70247911b026a2
DevSharpknife <davidjamesmurphy@gmail.com>
2020-09-08 21:09:19 +0100

    Removed original function and made a multiple parameter function

--- my_functions.py
+++ my_functions.py
@@ -1,7 +1,15 @@
-def greet(name): # MODIFIED
-    return "Hey " + name
+# def greet(name): # MODIFIED
+#     return "Hey " + name
 
-greeting = greet("Rowan") # ADDED
-print(greeting) # ADDED
+# greeting = greet("Rowan") # ADDED
+# print(greeting) # ADDED
 
-# => Hey Rowan
+# # => Hey Rowan
+
+def greet(name, time_of_day):
+    return "Good " + time_of_day + ", " + name
+
+greeting = greet("Rowan", "morning")
+print(greeting)
+
+# => "Good morning, Rowan"
